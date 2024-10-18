cc8af929cb0bf88fbab4c47f39213465e1be1a4b
Jonathan Evans <JonathanEvansDev@gmail.com>
2024-10-18 11:20:22 -0500

    Implement Game of Life Rules and Blinker Test
    There's a bug in the implementation right now which causes the rules to
    not propagate correctly. More work needs to be done to debug them

---
+++ src/board.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+
+template <std::size_t I, std::size_t J, std::size_t N = 0> struct Board;
+
+template <std::size_t I, std::size_t J, std::size_t N> struct GetNeighborCount {
+  static constexpr auto neighborCount =
+      static_cast<std::size_t>(Board<I + 1, J, N>::isAlive) +
+      static_cast<std::size_t>(Board<I + 1, J + 1, N>::isAlive) +
+      static_cast<std::size_t>(Board<I, J + 1, N>::isAlive) +
+      static_cast<std::size_t>(Board<I + 1, J, N>::isAlive) +
+      static_cast<std::size_t>(Board<I, J - 1, N>::isAlive) +
+      static_cast<std::size_t>(Board<I - 1, J, N>::isAlive) +
+      static_cast<std::size_t>(Board<I - 1, J, N>::isAlive) +
+      static_cast<std::size_t>(Board<I - 1, J - 1, N>::isAlive);
+};
+// dead rule
+template <std::size_t I, std::size_t J, std::size_t N, bool isAlive>
+struct ShouldLiveImpl {
+  static constexpr auto neighborCount =
+      GetNeighborCount<I, J, N>::neighborCount;
+  static constexpr auto value = (neighborCount == 3);
+};
+
+// live rule
+template <std::size_t I, std::size_t J, std::size_t N>
+struct ShouldLiveImpl<I, J, N, true> {
+  static constexpr auto neighborCount =
+      GetNeighborCount<I, J, N>::neighborCount;
+  static constexpr auto value = (neighborCount == 2 || neighborCount == 3);
+};
+
+template <std::size_t I, std::size_t J, std::size_t N, bool isAlive>
+using ShouldLive = ShouldLiveImpl<I, J, N, isAlive>;
+
+template <std::size_t I, std::size_t J, std::size_t N> struct Board {
+  static constexpr auto isAlive =
+      ShouldLive<I, J, N - 1, Board<I, J, N - 1>::isAlive>::value;
+};
+
+template <std::size_t I, std::size_t J> struct Board<I, J, 0> {
+  static constexpr auto isAlive = false;
+};
+
+// Board definition
+
+template <> struct Board<0, 1, 0> {
+  static constexpr auto isAlive = true;
+};
+
+template <> struct Board<1, 1, 0> {
+  static constexpr auto isAlive = true;
+};
+
+template <> struct Board<2, 1, 0> {
+  static constexpr auto isAlive = true;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
-int main(int argc, char const *argv[])
-{
-    return 0;
+#include "board.hpp"
+
+#include <iostream>
+
+char getBlock(const bool value) { return value ? 'x' : ' '; }
+
+template <std::size_t Epoch> void printBoard() {
+  std::cout << getBlock(Board<0, 2, Epoch>::isAlive)
+            << getBlock(Board<1, 2, Epoch>::isAlive)
+            << getBlock(Board<2, 2, Epoch>::isAlive) << "\n"
+            << getBlock(Board<0, 1, Epoch>::isAlive)
+            << getBlock(Board<1, 1, Epoch>::isAlive)
+            << getBlock(Board<2, 1, Epoch>::isAlive) << "\n"
+            << getBlock(Board<0, 0, Epoch>::isAlive)
+            << getBlock(Board<1, 0, Epoch>::isAlive)
+            << getBlock(Board<2, 0, Epoch>::isAlive) << "\n";
 }
+
+int main(int argc, char const *argv[]) { printBoard<2>(); }
